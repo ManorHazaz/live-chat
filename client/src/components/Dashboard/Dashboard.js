60afc0af320b738236ce3f29f9901bcd495dc6b5
manor hagage <manorhagage@gmail.com>
2021-02-07 15:24:22 +0200

    Add and style dashboard component

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import { Sidebar } from '../Sidebar'
+import './Dashboard.css'
+
+function Dashboard() {
+    return (
+        <div className='dashboard'>
+            <Sidebar />
+            
+        </div>
+    )
+}
+
+export default Dashboard
