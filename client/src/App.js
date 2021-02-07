3f8c95fd6c67546af12976a47686fd67764a10b1
manor hagage <manorhagage@gmail.com>
2021-02-07 15:06:25 +0200

    Create and style login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Login from './components/Login/Login';
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
+		
+			<Login />
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,7 +3,9 @@ import './Login.css';
 function Login() {
     return (
         <div className='login'>
-            
+            <h1> Live Chat </h1>
+            <input type='text' className='text-input' placeholder='username'/>
+            <input type='submit' className='btn' value='Create User'/>
         </div>
     )
 }
